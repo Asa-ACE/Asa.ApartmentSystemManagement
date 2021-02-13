473cc23305b53452d875481f32dc64d47117abb8
Omid <omid.panakari.s@gmail.com>
2021-02-13 09:23:08 +0330

    more DTO added

---
+++ Back-End/Asa.ApartmentSystemManagement/Asa.ApartmentSystemManagement.Core/BaseInfo/DTOs/UnitDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asa.ApartmentSystemManagement.Core.BaseInfo.DTOs
+{
+	public class UnitDTO
+	{
+		public int Id { get; set; }
+		public int BuildingId { get; set; }
+		public decimal Area { get; set; }
+		public int UnitNumber { get; set; }
+		public string Description { get; set; }
+	}
+}
