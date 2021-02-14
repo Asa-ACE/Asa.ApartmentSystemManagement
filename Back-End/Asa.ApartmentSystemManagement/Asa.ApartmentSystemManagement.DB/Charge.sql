97f588a23172b34ffc0f0a1a690f797f9cdecbd8
Amirhosein <312.amirhosein@gmail.com>
2021-02-14 09:42:16 +0330

    All tables added

---
+++ Back-End/Asa.ApartmentSystemManagement/Asa.ApartmentSystemManagement.DB/Charge.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Charge]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ExpanseID] INT NOT NULL, 
+    [PersonID] INT NOT NULL, 
+    [money] DECIMAL NOT NULL, 
+    [Payment] DECIMAL NOT NULL
+)
