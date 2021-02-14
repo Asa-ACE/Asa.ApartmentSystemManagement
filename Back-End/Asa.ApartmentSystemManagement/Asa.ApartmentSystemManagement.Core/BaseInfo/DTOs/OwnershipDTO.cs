82a3174bcc50f32bcbf4b5e99f8e74564391e4f6
Omid <omid.panakari.s@gmail.com>
2021-02-14 09:39:50 +0330

    added rest of the dtos.

---
+++ Back-End/Asa.ApartmentSystemManagement/Asa.ApartmentSystemManagement.Core/BaseInfo/DTOs/OwnershipDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asa.ApartmentSystemManagement.Core.BaseInfo.DTOs
+{
+	public class OwnershipDTO
+	{
+		public int OwnershipId { get; set; }
+		public int PersonId { get; set; }
+		public int UnitId { get; set; }
+		public DateTime From { get; set; }
+		public DateTime? To { get; set; }
+	}
+}
