fdcf6ef8dace22846804781edb8c8b177898ef54
maryam rja <rajabi.asl.maryam@gmail.com>
2021-02-19 17:29:02 +0330

    edited baseInfo

--- Back-End/Asa.ApartmentSystemManagement/Asa.ApartmentSystemManagement.ApplicationServices/BaseInfoApplicationService.cs
+++ Back-End/Asa.ApartmentSystemManagement/Asa.ApartmentSystemManagement.ApplicationServices/BaseInfoApplicationService.cs
@@ -1,8 +1,27 @@
-﻿using System;
+﻿using Asa.ApartmentSystemManagement.Core.BaseInfo.DTOs;
+using Asa.ApartmentSystemManagement.Core.BaseInfo.Gateways;
+using Asa.ApartmentSystemManagement.Core.BaseInfo.Managers;
+using Asa.ApartmentSystemManagement.Infra.DataGateways;
+using System;
+using System.Threading.Tasks;
 
 namespace Asa.ApartmentSystemManagement.ApplicationServices
 {
 	public class BaseInfoApplicationService
 	{
-	}
+		ITableGatewayFactory tableGatewayFactory;
+
+        public BaseInfoApplicationService(string connectionString)
+        {
+			tableGatewayFactory = new SqlTableGatewayFactory(connectionString);
+        }
+
+        public async Task<int> CreateBuilding(string name, int numberofUnits)
+        {
+            BuildingManager buildingManager = new BuildingManager(tableGatewayFactory);
+            var buildingDto = new BuildingDTO { Name = name, NumberOfUnits = numberofUnits };
+            await buildingManager.AddBuilding(buildingDto);
+            return buildingDto.Id;
+        }
+    }
 }
