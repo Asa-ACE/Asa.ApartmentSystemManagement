ed9378649a428d5605e4df11c9b013f99bf806e9
maryam rja <rajabi.asl.maryam@gmail.com>
2021-02-19 18:23:54 +0330

    added unit get procedure.

---
+++ Back-End/Asa.ApartmentSystemManagement/Asa.ApartmentSystemManagement.DB/dbo/StoredProcedures/unit_get.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[unit_get]
+	@building_id int
+AS
+BEGIN
+	SELECT [BuildingID], [UnitID], [Area], [Number], [Description]
+	FROM [dbo].[Unit]
+	WHERE [BuildingID] = @building_id
+END
