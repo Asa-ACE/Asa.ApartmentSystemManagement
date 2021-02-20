e42a16be45d5869c9ae48f4fa836464e67fc9c16
Amirhosein <312.amirhosein@gmail.com>
2021-02-20 16:19:42 +0330

    tab panel added

--- Front-End/apartmentmanagement/src/App.js
+++ Front-End/apartmentmanagement/src/App.js
@@ -6,12 +6,12 @@ import Login from "./components/Login";
 import Register from "./components/Register";
 import BuildingPage from "./components/buildingPage/BuildingPage";
 import UnitTable from "./components/unitsPage/UnitTable";
+import TabsContainer from "./components/TabsContainer"
 
 function App() {
   return (
     <>
-      <Navbar />
-      <UnitTable />
+      <TabsContainer/>
     </>
   );
 }
---
+++ Front-End/apartmentmanagement/src/components/TabsContainer.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Tab, Tabs } from "@material-ui/core";
+import TabPanel from "./TabPanel";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    display: "flex",
+  },
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`,
+  },
+}));
+
+function TabsContainer(props) {
+  const classes = useStyles();
+  //const { tabs } = props;
+  const tabs = [{ name: "ali", element: Tab }];
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        className={classes.tabs}
+      >
+        {tabs.map((tab, index) => (
+          <Tab
+            label={tab.name}
+            {...{
+              id: `vertical-tab-`,
+              "aria-controls": `vertical-tabpanel-`,
+            }}
+          />
+        ))}
+      </Tabs>
+      ;
+      {tabs.map((tab, index) => (
+        <TabPanel value={value} index={index}>
+          {tab.element}
+        </TabPanel>
+      ))}
+    </div>
+  );
+}
+
+export default TabsContainer;
