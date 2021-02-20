0e59e9b725e11f12888ba4fa1c1a87eaec6af797
Amirhosein <312.amirhosein@gmail.com>
2021-02-20 09:52:38 +0330

    navbar updated

--- Front-End/apartmentmanagement/src/components/Navbar.jsx
+++ Front-End/apartmentmanagement/src/components/Navbar.jsx
@@ -6,6 +6,12 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import Grow from "@material-ui/core/Grow";
+import Paper from "@material-ui/core/Paper";
+import Popper from "@material-ui/core/Popper";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuList from "@material-ui/core/MenuList";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,10 +23,39 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+  profile: {
+    color: "white",
+  },
 }));
 
-function Navbar() {
+function Navbar(props) {
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+
+  const handleToggle = () => {
+    console.log(open);
+    setOpen((prevOpen) => !prevOpen);
+    console.log(open);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+
   return (
     <AppBar position="static">
       <Toolbar>
@@ -32,9 +67,49 @@ function Navbar() {
         >
           <MenuIcon />
         </IconButton>
-        <Typography variant="h6" className={classes.title}>
+        <Typography variant="h4" className={classes.title}>
           شارژآسا
         </Typography>
+        <Button
+          aria-controls={open ? "menu-list-grow" : undefined}
+          aria-haspopup="true"
+          onClick={() => {
+            console.log("Didid");
+            handleToggle();
+          }}
+          ref={anchorRef}
+        >
+          <Typography variant="h6" className={classes.profile}>
+            امیرحسین
+          </Typography>
+        </Button>
+        <Popper
+          open={open}
+          anchorEl={anchorRef.current}
+          role={undefined}
+          transition
+          disablePortal
+        >
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{
+                transformOrigin:
+                  placement === "bottom" ? "center top" : "center bottom",
+              }}
+            >
+              <Paper>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList autoFocusItem={open} id="menu-list-grow">
+                    <MenuItem onClick={handleClose}>Profile</MenuItem>
+                    <MenuItem onClick={handleClose}>My account</MenuItem>
+                    <MenuItem onClick={handleClose}>Logout</MenuItem>
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
       </Toolbar>
     </AppBar>
   );
