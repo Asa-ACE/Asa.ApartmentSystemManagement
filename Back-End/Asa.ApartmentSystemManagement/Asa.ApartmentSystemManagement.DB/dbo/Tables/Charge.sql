79d5b425d96ac780461b05bceff0e1709efa1d79
Amirhosein <312.amirhosein@gmail.com>
2021-02-21 10:24:10 +0330

    All foreign keys added

--- Back-End/Asa.ApartmentSystemManagement/Asa.ApartmentSystemManagement.DB/dbo/Tables/Charge.sql
+++ Back-End/Asa.ApartmentSystemManagement/Asa.ApartmentSystemManagement.DB/dbo/Tables/Charge.sql
@@ -4,5 +4,7 @@
     [ExpanseID] INT NOT NULL, 
     [PersonID] INT NOT NULL, 
     [money] DECIMAL NOT NULL, 
-    [Payment] DECIMAL NOT NULL
+    [Payment] DECIMAL NOT NULL, 
+    CONSTRAINT [FK_Charge_Person] FOREIGN KEY ([PersonID]) REFERENCES [Person]([PersonID]), 
+    CONSTRAINT [FK_Charge_Expanse] FOREIGN KEY ([ExpanseID]) REFERENCES [Expanse]([ExpanseID])
 )
--- Back-End/Asa.ApartmentSystemManagement/Asa.ApartmentSystemManagement.DB/dbo/Tables/Expanse.sql
+++ Back-End/Asa.ApartmentSystemManagement/Asa.ApartmentSystemManagement.DB/dbo/Tables/Expanse.sql
@@ -7,5 +7,6 @@
     [To] DATE NOT NULL, 
     [Amount] DECIMAL NOT NULL, 
     [Name] NVARCHAR(50) NOT NULL, 
-    CONSTRAINT [FK_Expanse_Category] FOREIGN KEY ([CategoryID]) REFERENCES [ExpanseCategory]([CategoryID])
+    CONSTRAINT [FK_Expanse_Category] FOREIGN KEY ([CategoryID]) REFERENCES [ExpanseCategory]([CategoryID]), 
+    CONSTRAINT [FK_Expanse_Building] FOREIGN KEY ([BuildingID]) REFERENCES [Building]([BuildingID])
 )
