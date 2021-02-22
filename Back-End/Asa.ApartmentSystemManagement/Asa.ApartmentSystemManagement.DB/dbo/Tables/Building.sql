0f359928f101c4e9c954fa3b5aa121ba8037b560
Amirhosein <312.amirhosein@gmail.com>
2021-02-22 13:04:03 +0330

    tables updated

--- Back-End/Asa.ApartmentSystemManagement/Asa.ApartmentSystemManagement.DB/dbo/Tables/Building.sql
+++ Back-End/Asa.ApartmentSystemManagement/Asa.ApartmentSystemManagement.DB/dbo/Tables/Building.sql
@@ -2,6 +2,7 @@
 (
 	[BuildingID] int identity(1,1) PRIMARY KEY NOT null,
 	[Name] nvarchar(50) NOT null,
-	[NumberOfUnits] SMALLINT NOT null
+	[NumberOfUnits] SMALLINT NOT null, 
+    [Address] NVARCHAR(200) NOT NULL UNIQUE
 )
 GO
--- Back-End/Asa.ApartmentSystemManagement/Asa.ApartmentSystemManagement.DB/dbo/Tables/ExpanseCategory.sql
+++ Back-End/Asa.ApartmentSystemManagement/Asa.ApartmentSystemManagement.DB/dbo/Tables/ExpanseCategory.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[ExpanseCategory]
 (
 	[CategoryID] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Name] NVARCHAR(50) NOT NULL, 
+    [Name] NVARCHAR(50) NOT NULL UNIQUE, 
     [FormulaType] TINYINT NOT NULL, --there are just 5 FormulaType
     [IsForOwner] BIT NOT NULL
 )
