f38a30f275f56e3683194b1bc6e0bcd28672c990
Amirhosein <312.amirhosein@gmail.com>
2021-02-22 11:22:05 +0330

    a bog fixed

--- Back-End/Asa.ApartmentSystemManagement/Asa.ApartmentSystemManagement.DB/dbo/Tables/Person.sql
+++ Back-End/Asa.ApartmentSystemManagement/Asa.ApartmentSystemManagement.DB/dbo/Tables/Person.sql
@@ -5,6 +5,6 @@
 	[LastName] nvarchar(20) not null,
 	[PhoneNumber] CHAR(11) UNIQUE not null, 
     [UserName] VARCHAR(50) UNIQUE NOT NULL, 
-    [Password] VARCHAR(50) UNIQUE NOT NULL
+    [Password] VARCHAR(50) NOT NULL
 )
 GO
