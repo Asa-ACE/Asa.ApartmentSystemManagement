8e4017cbcc6dd5f8345d7e08954192aa2f95682f
maryam <rajabi.asl.maryam@gmail.com>
2021-02-22 13:43:11 +0330

    fixed some conflicts.

--- Back-End/Asa.ApartmentSystemManagement/Asa.ApartmentSystemManagement.DB/dbo/Tables/ChargeItem.sql
+++ Back-End/Asa.ApartmentSystemManagement/Asa.ApartmentSystemManagement.DB/dbo/Tables/ChargeItem.sql
@@ -6,6 +6,6 @@
     [Amount] INT NOT NULL, 
     [UnitID] INT NOT NULL, 
     CONSTRAINT [FK_ChargeItem_Person] FOREIGN KEY ([PersonID]) REFERENCES [Person]([PersonID]), 
-    CONSTRAINT [FK_ChargeItem_Expanse] FOREIGN KEY ([ExpanseID]) REFERENCES [Expanse]([ExpanseID]), 
+    CONSTRAINT [FK_ChargeItem_Expanse] FOREIGN KEY ([ExpanseID]) REFERENCES [Expense]([ExpenseID]), 
     CONSTRAINT [FK_ChargeItem_Unit] FOREIGN KEY ([UnitID]) REFERENCES [Unit]([UnitID])
 )
