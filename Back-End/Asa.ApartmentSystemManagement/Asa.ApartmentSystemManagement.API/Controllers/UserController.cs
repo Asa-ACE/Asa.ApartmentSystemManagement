dbfb9ba48ad88c41c6472d4b05f3efc3c1ee93fe
Omid <Asus@LAPTOP-QP6PFO6G>
2021-02-25 10:15:58 +0330

    user controller added

---
+++ Back-End/Asa.ApartmentSystemManagement/Asa.ApartmentSystemManagement.API/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Asa.ApartmentSystemManagement.API.Interfaces.ApplicationServices;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asa.ApartmentSystemManagement.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private IBaseInfoApplicationService _baseInfoApplicationService;
+        public UserController(IBaseInfoApplicationService baseInfoApplicationService)
+        {
+            _baseInfoApplicationService = baseInfoApplicationService;
+        }
+    }
+}
