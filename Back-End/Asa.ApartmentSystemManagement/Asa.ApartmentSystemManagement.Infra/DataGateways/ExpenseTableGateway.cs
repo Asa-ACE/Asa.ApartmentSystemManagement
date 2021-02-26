30fe09352c9d12c3fad12c64231243f07528aa7f
amirhoseinmohseni <312.amirhosein@gmail.com>
2021-02-26 14:32:56 +0330

    GetExpensesByBuildingIdAndDate added to gateway

--- Back-End/Asa.ApartmentSystemManagement/Asa.ApartmentSystemManagement.Infra/DataGateways/ExpenseTableGateway.cs
+++ Back-End/Asa.ApartmentSystemManagement/Asa.ApartmentSystemManagement.Infra/DataGateways/ExpenseTableGateway.cs
@@ -2,6 +2,7 @@
 using Asa.ApartmentSystemManagement.Core.BaseInfo.Gateways;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,9 +22,44 @@ namespace Asa.ApartmentSystemManagement.Infra.DataGateways
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<ExpenseDTO>> GetExpensesByBuildingIdAndDate(DateTime from, DateTime to, int buildingId)
+        public async Task<IEnumerable<ExpenseDTO>> GetExpensesByBuildingIdAndDate(DateTime from, DateTime to, int buildingId)
         {
-            throw new NotImplementedException();
+            var result = new List<ExpenseDTO>();
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                using (var cmd = new SqlCommand())
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.CommandText = "[dbo].[SpExpenseGetByDate]";
+                    cmd.Parameters.AddWithValue("@buildingId", buildingId);
+                    cmd.Parameters.AddWithValue("@from", from);
+                    cmd.Parameters.AddWithValue("@to", to);
+                    cmd.Connection = connection;
+                    cmd.Connection.Open();
+                    using (var dataReader = await cmd.ExecuteReaderAsync())
+                    {
+                        while (await dataReader.ReadAsync())
+                        {
+                            var expenseDTO = new ExpenseDTO();
+                            //unitDTO.BuidlingId = Convert.ToInt32(dataReader["building_id"]);
+                            //unitDTO.Id= Convert.ToInt32(dataReader["id"]);
+                            //unitDTO.Number= Convert.ToInt32(dataReader["number"]);
+                            //unitDTO.Area= Convert.ToDecimal(dataReader["area"]);
+                            //unitDTO.Description= Convert.ToString(dataReader["description"]);
+                            expenseDTO.Amount = Convert.ToDecimal(dataReader["Amount"]);
+                            expenseDTO.BuildingId = Convert.ToInt32(dataReader["BuildingID"]);
+                            expenseDTO.CategoryId = Convert.ToInt32(dataReader["CategoryID"]);
+                            expenseDTO.ExpenseId = Convert.ToInt32(dataReader["ExpenseID"]);
+                            expenseDTO.From = Convert.ToDateTime(dataReader["From"]);
+                            expenseDTO.To = Convert.ToDateTime(dataReader["To"]);
+                            expenseDTO.Name = Convert.ToString(dataReader["Name"]);
+                            result.Add(expenseDTO);
+                        }
+                    }
+                }
+            }
+            return result;
         }
 
         public Task<int> InsertExpenseAsync(ExpenseDTO expense)
