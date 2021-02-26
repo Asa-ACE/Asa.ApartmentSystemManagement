a97a98a16691b9f99c9662b4c42a2dd69d0707a4
amirhoseinmohseni <312.amirhosein@gmail.com>
2021-02-26 10:09:23 +0330

    AreaAndPeopelBasedFormula added

---
+++ Back-End/Asa.ApartmentSystemManagement/Asa.ApartmentSystemManagement.Core/ChargeCalculation/Domain/CalculationFormula/AreaAndPeopelBasedFormula.cs
@@ -0,0 +1,33 @@
+﻿using Asa.ApartmentSystemManagement.Core.BaseInfo.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASa.ApartmentManagement.Core.ChargeCalculation.Domain.CalculationFormula
+{
+    [CalculationFormula("بر اساس مساحت و تعداد نفرات")]
+    public class AreaAndPeopelBasedFormula : IFormula
+    {
+        public List<ChargeItemDTO> Calculate(decimal amount, IEnumerable<ShareInfo> shareInfos, int expenseId, bool isOwner)
+        {
+            decimal sum = 0;
+            List<ChargeItemDTO> chargeItems = null;
+
+            foreach (var shareInfo in shareInfos)
+            {
+                sum += shareInfo.Days * shareInfo.NumberOfPeopel * shareInfo.Area;
+            }
+            foreach (var shareInfo in shareInfos)
+            {
+                var chargeItem = new ChargeItemDTO();
+                chargeItem.Amount = amount * ((shareInfo.Days * shareInfo.NumberOfPeopel * shareInfo.Area) / sum);
+                chargeItem.ExpenseId = expenseId;
+                chargeItem.PersonId = shareInfo.PersonId;
+                chargeItem.UnitId = shareInfo.UnitId;
+                chargeItem.IsOwner = isOwner;
+            }
+
+            return chargeItems;
+        }
+    }
+}
