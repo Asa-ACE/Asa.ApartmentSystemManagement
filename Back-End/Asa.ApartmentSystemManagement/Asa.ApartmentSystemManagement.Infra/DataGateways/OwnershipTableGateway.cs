1bcfbe97d049ef6dc09e7696be9cfd1022e0ac5e
amirhoseinmohseni <312.amirhosein@gmail.com>
2021-02-27 17:33:58 +0330

    SpOwnershipGetShareInfo added

--- Back-End/Asa.ApartmentSystemManagement/Asa.ApartmentSystemManagement.Infra/DataGateways/OwnershipTableGateway.cs
+++ Back-End/Asa.ApartmentSystemManagement/Asa.ApartmentSystemManagement.Infra/DataGateways/OwnershipTableGateway.cs
@@ -17,7 +17,7 @@ namespace Asa.ApartmentSystemManagement.Infra.DataGateways
             _connectionString = connectionString;
         }
 
-        public Task<IEnumerable<ShareInfo>> GetOwnerPaymentsAsync(int UnitId, DateTime from, DateTime to)
+        public Task<IEnumerable<ShareInfo>> GetOwnerShareInfoAsync(int UnitId, DateTime from, DateTime to)
         {
             throw new NotImplementedException();
         }
