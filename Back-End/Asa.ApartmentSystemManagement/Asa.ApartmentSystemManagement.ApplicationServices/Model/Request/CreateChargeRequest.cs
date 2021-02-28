f1e03dbb2672e5c0ec1888c280dc2b6f093d0eb6
Omid <omid.panakari.s@gmail.com>
2021-02-28 09:01:36 +0330

    create charge added

---
+++ Back-End/Asa.ApartmentSystemManagement/Asa.ApartmentSystemManagement.ApplicationServices/Model/Request/CreateChargeRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asa.ApartmentSystemManagement.ApplicationServices.Model.Request
+{
+	public class CreateChargeRequest
+	{
+		public int BuildingId { get; set; }
+		public DateTime From { get; set; }
+		public DateTime To { get; set; }
+	}
+}
---
+++ Back-End/Asa.ApartmentSystemManagement/Asa.ApartmentSystemManagement.Core/ChargeCalculation/Domain/CalculationFormula/ConstantFormula.cs
@@ -0,0 +1,35 @@
+﻿using Asa.ApartmentSystemManagement.Core.BaseInfo.DTOs;
+using ASa.ApartmentManagement.Core.ChargeCalculation.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asa.ApartmentSystemManagement.Core.ChargeCalculation.Domain.CalculationFormula
+{
+	public class ConstantFormula: IFormula
+	{
+        public List<ChargeItemDTO> Calculate(decimal amount, IEnumerable<ShareInfo> shareInfos, int expenseId, bool isOwner)
+        {
+            Dictionary<int, decimal> daysSum = new Dictionary<int, decimal>();
+            List<ChargeItemDTO> chargeItems = null;
+            foreach (var shareInfo in shareInfos)
+            {
+                if (!daysSum.ContainsKey(shareInfo.UnitId))
+                    daysSum.Add(shareInfo.UnitId, 0);
+                daysSum[shareInfo.UnitId] += shareInfo.Days;
+            }
+            foreach (var shareInfo in shareInfos)
+            {
+                var chargeItem = new ChargeItemDTO();
+                chargeItem.Amount = amount * (shareInfo.Days / daysSum[shareInfo.UnitId]);
+                chargeItem.ExpenseId = expenseId;
+                chargeItem.PersonId = shareInfo.PersonId;
+                chargeItem.UnitId = shareInfo.UnitId;
+                chargeItem.IsOwner = isOwner;
+            }
+
+            return chargeItems;
+        }
+
+	}
+}
